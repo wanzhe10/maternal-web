0a4c2492336077ee76adc05f703cd2cdb201bb90
wanzhe <zhe_123_yeah.net@163.com>
2018-10-17 18:59:24 +0800

    '新建档案'

--- newFile/newFile.js
+++ newFile/newFile.js
@@ -1,13 +1,10 @@
-$(function(){
-  $('.newBtn').click(function(){
-      window.location = '/maternal-web/newFile/newFile.html';
-  });
- layui.use(['layer', 'form', 'element', 'laydate'], function () {
-     var layer = layui.layer,
-         form = layui.form,
-         element = layui.element,
-         laydate = layui.laydate;
-     form.render();
+$(function () {
+    layui.use(['layer', 'form', 'element', 'laydate'], function () {
+        var layer = layui.layer,
+            form = layui.form,
+            element = layui.element,
+            laydate = layui.laydate;
+        form.render();
         layui.use('laypage', function () {
             var laypage = layui.laypage;
             //执行一个laypage实例
@@ -18,12 +15,9 @@ $(function(){
                 limit: 7, // 每页的条数
                 groups: 4,
                 jump: function (obj, first) {
-                   
-                }
 
+                }
             });
         });
-
- });
-
+    });
 });
